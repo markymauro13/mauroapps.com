2974082e54ec9e62586aef62bb0731c00757da8c
Kenneth Doerflein <98554038+KennethDoerflein@users.noreply.github.com>
2024-09-24 00:14:45 -0400

    Fix id in SolutionsHero.jsx

--- src/components/Solutions/SolutionsHero.jsx
+++ src/components/Solutions/SolutionsHero.jsx
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 export default function AboutHero() {
   return (
     <>
-      <Container fluid className="content-wrapper" id="about">
+      <Container fluid className="content-wrapper" id="solutions">
         <h1 className="main-title">Custom Software Solutions for your Business</h1>
         <p className="subtitle">Our code does the heavy lifting!</p>
       </Container>
