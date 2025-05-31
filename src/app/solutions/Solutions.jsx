b750da4bfb94950605120d0b7170909b4579a537
Kenneth Doerflein <98554038+KennethDoerflein@users.noreply.github.com>
2025-05-31 00:13:56 -0400

    Fix solutions page routing

--- src/app/solutions/Solutions.jsx
+++ src/app/solutions/Solutions.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import SolutionsHero from "./SolutionsHero";
 import AboutSection from "./SolutionSection/AboutSection";
+import SolutionsHero from "../Solutions/SolutionsHero";
 export default function About() {
   return (
     <>
