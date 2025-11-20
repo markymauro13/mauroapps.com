532a896ada272d2c1602344577d9713e9da86299
Mark Mauro <markymauro@gmail.com>
2025-11-20 13:55:07 -0500

    editing email contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,11 +9,11 @@ export default function Contact() {
         <Row className="justify-content-center">
           <Col lg="8" className="text-center">
             <h1 className="main-title">Contact Us</h1>
-              <div className="contact-info">
+            <div className="contact-info">
               <p className="lead">
-                email us @{" "}
-                <a href="mailto:markymauro@gmail.com" className="email-link">
-                  markymauro@gmail.com
+                email us at{" "}
+                <a href="mailto:mark@mauroapps.com" className="email-link">
+                  mark@mauroapps.com
                 </a>
               </p>
             </div>
